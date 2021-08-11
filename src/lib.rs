88660b58fcade1ea3075050bf8c0d1a61a86b87a
Jack O'Connor <oconnor663@gmail.com>
2021-08-11 18:45:34 -0400

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,33 @@
+#![no_std]
+
+use core::cell::Cell;
+
+#[macro_export]
+macro_rules! project {
+    ($e:ident $(. $field:tt)* ) => {{
+        let cell: &core::cell::Cell<_> = $e;
+        $(
+        let field_mut = unsafe { &mut (*cell.as_ptr()).$field };
+        let cell = core::cell::Cell::from_mut(field_mut);
+        )*
+        cell
+    }};
+}
+
+pub fn array_of_cells<T, const N: usize>(cell: &Cell<[T; N]>) -> &[Cell<T>; N] {
+    // SAFETY: `Cell<T>` has the same memory layout as `T`.
+    unsafe { &*(cell as *const Cell<[T; N]> as *const [Cell<T>; N]) }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array_of_cells() {
+        let cell = Cell::new([1, 2, 3]);
+        let array_of_cells: &[Cell<i32>; 3] = array_of_cells(&cell);
+        array_of_cells[0].set(99);
+        assert_eq!(cell.into_inner(), [99, 2, 3]);
+    }
+}
