809985f1de40e7c735d27bd3a57fd18016867e20
Jack O'Connor <oconnor663@gmail.com>
2021-08-11 19:03:59 -0400

    add docs

---
+++ README.md
@@ -0,0 +1,39 @@
+# `cell_utils`
+
+This Rust crate contains two utilities for working with `Cell`:
+`array_of_cells` and `project!`.
+
+The `array_of_cells` function converts between `&Cell<[T; N]>` and `&[Cell<T>;
+N]`. This is very similar to `Cell::as_slice_of_cells`, and I've [proposed
+adding it to the standard
+library](https://github.com/rust-lang/rust/pull/87944).
+
+```rust
+use cell_utils::array_of_cells;
+
+let cell: Cell<[i32; 3]> = Cell::new([1, 2, 3]);
+let array: &[Cell<i32>; 3] = array_of_cells(&cell);
+array[0].set(99);
+assert_eq!(cell.into_inner(), [99, 2, 3]);
+```
+
+The `project!` macro lets you access the fields of a struct or the elements of
+a tuple, given a reference to a containing cell.
+
+```rust
+use cell_utils::project;
+
+struct Foo {
+    bar: Bar,
+}
+
+struct Bar {
+    baz: i32,
+}
+
+let mut foo = Foo { bar: Bar { baz: 42 } };
+let foo_cell: &Cell<Foo> = Cell::from_mut(&mut foo);
+let baz_cell: &Cell<i32> = project!(foo_cell.bar.baz);
+baz_cell.set(99);
+assert_eq!(foo.bar.baz, 99);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,100 @@
+//! This crate provides two utilities for working with cell
+//! [`Cell`](https://doc.rust-lang.org/std/cell/struct.Cell.html), the `array_of_cells` function
+//! and the `project!` macro.
+//!
+//! # Examples
+//!
+//! ```
+//! # use cell_utils::{array_of_cells, project};
+//! # use core::cell::Cell;
+//! ///
+//! /// Convert a reference to a cell-of-array into a reference to an array-of-cells.
+//! ///
+//!
+//! let cell: Cell<[i32; 3]> = Cell::new([1, 2, 3]);
+//! let array: &[Cell<i32>; 3] = array_of_cells(&cell);
+//! array[0].set(99);
+//! assert_eq!(cell.into_inner(), [99, 2, 3]);
+//!
+//! ///
+//! /// Extract references to struct fields inside a cell.
+//! /// Note that the extracted references are also cells.
+//! ///
+//!
+//! struct Foo {
+//!     bar: Bar,
+//! }
+//! struct Bar {
+//!     baz: i32,
+//! }
+//! let mut foo = Foo { bar: Bar { baz: 42 } };
+//! let foo_cell: &Cell<Foo> = Cell::from_mut(&mut foo);
+//! let baz_cell: &Cell<i32> = project!(foo_cell.bar.baz);
+//! baz_cell.set(99);
+//! assert_eq!(foo.bar.baz, 99);
+//! ```
+
 #![no_std]
 
 use core::cell::Cell;
 
+/// Given a reference to a [`Cell`](https://doc.rust-lang.org/std/cell/struct.Cell.html) containing
+/// an array, return a reference to an array of cells.
+///
+/// Internally this is a pointer cast, with no runtime cost.
+///
+/// This is very similar to the standard
+/// [`Cell::as_slice_of_cells`](https://doc.rust-lang.org/std/cell/struct.Cell.html#method.as_slice_of_cells)
+/// method.
+///
+/// # Example
+///
+/// ```
+/// # use cell_utils::array_of_cells;
+/// # use core::cell::Cell;
+/// let cell: Cell<[i32; 3]> = Cell::new([1, 2, 3]);
+/// let array: &[Cell<i32>; 3] = array_of_cells(&cell);
+/// array[0].set(99);
+/// assert_eq!(cell.into_inner(), [99, 2, 3]);
+/// ```
+pub fn array_of_cells<T, const N: usize>(cell: &Cell<[T; N]>) -> &[Cell<T>; N] {
+    // SAFETY: `Cell<T>` has the same memory layout as `T`.
+    unsafe { &*(cell as *const Cell<[T; N]> as *const [Cell<T>; N]) }
+}
+
+/// Given a reference to a [`Cell`](https://doc.rust-lang.org/std/cell/struct.Cell.html) containing
+/// a struct or a tuple, return a reference one of the fields or elements of that object.
+///
+/// # Example
+///
+/// ```
+/// # use cell_utils::project;
+/// # use core::cell::Cell;
+/// struct Foo {
+///     bar: Bar,
+/// }
+/// struct Bar {
+///     baz: i32,
+/// }
+/// let mut foo = Foo { bar: Bar { baz: 42 } };
+/// let foo_cell: &Cell<Foo> = Cell::from_mut(&mut foo);
+/// let baz_cell: &Cell<i32> = project!(foo_cell.bar.baz);
+/// baz_cell.set(99);
+/// assert_eq!(foo.bar.baz, 99);
+/// ```
+///
+/// # Example
+///
+/// This also works with tuples:
+///
+/// ```
+/// # use cell_utils::project;
+/// # use core::cell::Cell;
+/// let mut tuple = ("foo", "bar", "baz");
+/// let tuple_cell: &Cell<_> = Cell::from_mut(&mut tuple);
+/// project!(tuple_cell.0).set("hello");
+/// assert_eq!(tuple.0, "hello");
+/// ```
 #[macro_export]
 macro_rules! project {
     ($e:ident $(. $field:tt)* ) => {{
@@ -12,11 +105,6 @@ macro_rules! project {
         )*
         cell
     }};
-}
-
-pub fn array_of_cells<T, const N: usize>(cell: &Cell<[T; N]>) -> &[Cell<T>; N] {
-    // SAFETY: `Cell<T>` has the same memory layout as `T`.
-    unsafe { &*(cell as *const Cell<[T; N]> as *const [Cell<T>; N]) }
 }
 
 #[cfg(test)]
