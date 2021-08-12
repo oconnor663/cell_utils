c290197ef7dc8d59e95767e4c518e971779877da
Jack O'Connor <oconnor663@gmail.com>
2021-08-12 14:15:49 -0400

    automatically take a reference inside project!()

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,22 @@
 //!
 //! # Examples
 //!
+//! Convert a reference to a cell-of-array into a reference to an array-of-cells:
 //! ```
-//! # use cell_utils::{array_of_cells, project};
+//! # use cell_utils::array_of_cells;
 //! # use core::cell::Cell;
-//! ///
-//! /// Convert a reference to a cell-of-array into a reference to an array-of-cells.
-//! ///
-//!
 //! let cell: Cell<[i32; 3]> = Cell::new([1, 2, 3]);
 //! let array: &[Cell<i32>; 3] = array_of_cells(&cell);
 //! array[0].set(99);
 //! assert_eq!(cell.into_inner(), [99, 2, 3]);
+//! ```
 //!
-//! ///
-//! /// Extract references to struct fields inside a cell.
-//! /// Note that the extracted references are also cells.
-//! ///
+//! Extract references to struct fields inside a cell. Note that the extracted references are also
+//! cells:
 //!
+//! ```
+//! # use cell_utils::project;
+//! # use core::cell::Cell;
 //! struct Foo {
 //!     bar: Bar,
 //! }
@@ -81,6 +80,24 @@ pub fn array_of_cells<T, const N: usize>(
 /// assert_eq!(foo.bar.baz, 99);
 /// ```
 ///
+/// `project!` will automatically take a reference if needed:
+///
+/// ```
+/// # use cell_utils::project;
+/// # use core::cell::Cell;
+/// # struct Foo {
+/// #     bar: Bar,
+/// # }
+/// # struct Bar {
+/// #     baz: i32,
+/// # }
+/// let foo_cell: Cell<_> = Cell::new(Foo { bar: Bar { baz: 42 } });
+/// // Note that foo_cell is not a reference. That's ok.
+/// let baz_cell: &Cell<i32> = project!(foo_cell.bar.baz);
+/// baz_cell.set(99);
+/// assert_eq!(foo_cell.into_inner().bar.baz, 99);
+/// ```
+///
 /// If you want to use any expression other than a bare variable name, you need to surround it with
 /// an extra set of parentheses:
 ///
@@ -111,9 +128,12 @@ pub fn array_of_cells<T, const N: usize>(
 #[macro_export]
 macro_rules! project {
     ($e:ident $(. $field:tt)* ) => {
-        project!(($e) $(. $field)*)
+        // Add a & automatically. If $e is already a reference, that's fine, because a double
+        // reference will get automatically dereferenced below.
+        project!((&$e) $(. $field)*)
     };
     (( $e:expr ) $(. $field:tt)* ) => {{
+        // If cell is a double reference, this automatically dereferences it.
         let cell: &core::cell::Cell<_> = $e;
         // SAFETY: We need this helper function to bind the lifetime of the reference.
         unsafe fn get_mut<T>(cell: &core::cell::Cell<T>) -> &mut T { &mut *cell.as_ptr() }
@@ -167,6 +187,17 @@ mod tests {
         let y = project!(x);
         y.set(6);
         assert_eq!(x.get(), 6);
+    }
+
+    #[test]
+    fn test_project_autoref() {
+        struct Foo {
+            x: i32,
+        }
+        let tuple = Cell::new((Foo { x: 0 }, Foo { x: 1 }));
+        // This automatically takes a reference.
+        project!(tuple.0.x).set(99);
+        assert_eq!(tuple.into_inner().0.x, 99);
     }
 
     #[test]
